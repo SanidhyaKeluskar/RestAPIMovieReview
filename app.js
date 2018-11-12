80393efca4e3a6fad62fc2cca1732d7f7e9aef34
sanidhya70 <ksanidhya007@gmail.com>
2018-11-11 20:59:04 -0500

    Succesful connection to MySql

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 const express= require('express')
 const app=express()
+const mysql=require('mysql')
 
 app.get("/",(req,res)=>{
     res.send("Hello from root")
 })
+
+app.get("/:moviename",(req,res)=>{
+    console.log("ncdnnf "+ req.params.moviename)
+
+    const connection=mysql.createConnection({
+        host:'localhost',
+        user:'root',
+        password:'Sanidhya@101',
+        database: 'moviedb' 
+    })
+    const queryString="select * from review_table  where movie_name=?"
+    connection.query(queryString,[req.params.moviename],(err,rows,fields)=>{
+        console.log("fetched users sucessfully")
+        res.json(rows)
+    })
+    
+}) 
 
 app.get("/users",(req,res)=>{
 var user1={firstname: "John", lastName: "Smith"}
