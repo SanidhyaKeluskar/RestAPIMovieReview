458493e354ebfd2684cc0de02e778d6b81dbce8e
sanidhya70 <ksanidhya007@gmail.com>
2018-11-13 02:57:46 -0500

    Integrated Redis for caching

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express= require('express')
 const app=express()
 const mysql=require('mysql')
 const bodyParser=require('body-parser')
+const redis=require('redis');
+
+const client= redis.createClient()
 
 app.use(bodyParser.urlencoded({extended:false}))
 
@@ -18,8 +21,7 @@ app.get("/",(req,res)=>{
     })
  }
 app.post('/review',(req,res)=>{
-   // console.log("in post") 
-  //  console.log(" "+req.body.moviename)
+
     const name=req.body.name
     const movieName=req.body.moviename
     const movieReview=req.body.moviereview
@@ -36,26 +38,63 @@ app.post('/review',(req,res)=>{
     })
 })
 
+
 app.get("/:moviename",(req,res)=>{
     console.log("ncdnnf "+ req.params.moviename)
+    const moviename=req.params.moviename
 
-    const connection=getConnection()
-    const queryString="select * from review_table  where movie_name=?"
-    connection.query(queryString,[req.params.moviename],(err,rows,fields)=>{
+    client.lrange(moviename, 0,-1,function(err,value){
         if(err){
-            console.log("SOmething wrong" + err)
-            res.end()
+            console.log("SOmething wrong" + err)}
+        if(value){
+            console.log("we have value in redis cache")
+            console.log(value)
+      
+            const movies=[]
+            value.forEach(element=>{
+                movies.push({
+                    movieName : moviename,
+                    movieReview : element
+                })
+                })
+            res.json(movies)
         }
+        else{
+            const connection=getConnection()
+            const queryString="select * from review_table  where movie_name=?"
+            connection.query(queryString,[req.params.moviename],(err,rows,fields)=>{
+                if(err){
+                    console.log("SOmething wrong" + err)
+                    res.end()
+                }
+        
+                const movies= rows.map((row)=>{
+                    return{ movieName : row.movie_name,
+                            movieReview : row.review}
+                })
+                console.log("fetched users sucessfully")
+                res.json(movies)
+                rows.forEach(element => {
+                    client.rpush(moviename,element.review,function(err){
+                        if(err){
+                            console.log("errorr")
+                        }
+                        console.log("pushed to redis")
+                    })
+                });
+                client.expire(moviename,20)
 
-        const movies= rows.map((row)=>{
-            return{ movieName : row.movie_name,
-                    movieReview : row.review}
-        })
-        console.log("fetched users sucessfully")
-        res.json(movies)
-    })
+            })
     
-}) 
+            
+        }
+        
+    })
+  
+    
+})
+
+
 
 app.get("/users",(req,res)=>{
 var user1={firstname: "John", lastName: "Smith"}
